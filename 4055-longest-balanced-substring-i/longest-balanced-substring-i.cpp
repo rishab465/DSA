54b13749ba418e2f41f3330344cbdb8cdb2d41a0
rishab465 <yrishab479@gmail.com>
2026-02-12 20:31:29 +0530

    Time: 231 ms (30.08%) | Memory: 41.7 MB (13.62%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -5,11 +5,21 @@ public:
         int mx = 0;
         for(int i=0;i<n;i++){
             unordered_map<char , int> mp;
+
+
             for(int j=i;j<n;j++){
                 mp[s[j]]++;
-            int firstValue = mp.begin()->second;
+
+
+            int firstValue = -1;
             bool valid = true;
+            bool first = true;
+
             for(auto it : mp){
+                if(first){
+                    firstValue = it.second;
+                    first = false;
+                }
                 if(it.second != firstValue ){
                     valid = false;
                 }
