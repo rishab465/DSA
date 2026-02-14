b35b97082b1482f7043b170e7a4ce42fb4275a30
rishab465 <yrishab479@gmail.com>
2026-02-14 12:24:44 +0530

    Time: 1517 ms (5.07%) | Memory: 21 MB (5.08%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+         int ans = 0;
+        unordered_map<int , int> map;
+        int count = n;
+        for(int i=0;i<n;i++){
+            map[nums[i]]++;
+
+            for(auto it:map){
+                if(map[nums[i]]>2){
+                    nums[i] = INT_MAX;
+                    count--;
+                }
+            }
+        }
+        sort(nums.begin(),nums.end());
+        return count;
+    }
+};
