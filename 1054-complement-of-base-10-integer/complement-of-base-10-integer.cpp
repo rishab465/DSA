d9613f735a7e7f3ea90d4d2baf0f7c89833ecddd
rishab465 <yrishab479@gmail.com>
2026-03-11 09:42:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (4.02%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        string bit = "";
+
+        while(n!=0){
+            int rem = n%2;
+            bit += to_string(rem);
+            n/=2;
+        }
+        
+        reverse(bit.begin() , bit.end());
+
+        for(int i=0;i<bit.length();i++){
+            if(bit[i]=='0'){
+                bit[i]='1';
+            }else{
+                bit[i]='0';
+            }
+        }
+
+        int value = 0;
+        for(int i=0;i<bit.length();i++){
+            value = value * 2 + (bit[i]-'0');
+
+        }
+
+        return value;
+
+    }
+};
