fa3e7dc4f6ca561cbe718254a00f5af2d60a293c
rishab465 <yrishab479@gmail.com>
2026-04-04 02:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.88%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+
+            if(s[i] =='(' || s[i]=='[' || s[i] == '{'){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                if(st.top() == '(' && s[i] == ')' || 
+                st.top() == '{' && s[i] == '}' ||
+                st.top() == '[' && s[i] == ']' ){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+};
