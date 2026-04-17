6983f991d372f08d9daa2277cd47b956e4f55f78
rishab465 <yrishab479@gmail.com>
2026-04-18 00:06:02 +0530

    Time: 3 ms (13.72%) | Memory: 18.3 MB (35.51%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
-
         int n = words.size();
-
-        int min1 = -1;
+        int ans1 = -1;
+        int cnt = 0;
+        for(int i=startIndex;i<n+startIndex;i++){
+            int idx = i % n;
+            
+            if( words[idx] == target ){
+                ans1 = cnt;
+                break;
+            }
+            cnt++;
+        }
+        int ans2 = -1;
         int i = startIndex;
-
-        
-        for(int step = 0; step < n; step++){
-            if(words[i] == target){
-                min1 = step;
+        int count = 0;
+        while(count<n){
+            int idx = (i+n) % n;
+            
+            if(words[idx] == target){
+                
+                ans2 = count;
                 break;
             }
-            i = (i + 1) % n;
+            count++;
+            i--;
+            
+
         }
-
-        int min2 = -1;
-        i = startIndex;
-
-        
-        for(int step = 0; step < n; step++){
-            if(words[i] == target){
-                min2 = step;
-                break;
-            }
-            i = (i - 1 + n) % n;
-        }
-
-        if(min1 == -1 && min2 == -1){
+        if(ans1 == -1 && ans2 == -1){
             return -1;
+        }else{
+            return min(ans1 , ans2);
         }
-
-        if(min1 == -1) return min2;
-        if(min2 == -1) return min1;
-
-        return min(min1, min2);
     }
 };
