e67f735c9b3c72db03902f351306af54fc41eb6a
rishab465 <yrishab479@gmail.com>
2026-05-29 19:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.48%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -4,25 +4,20 @@ public:
         int n = s.length();
         stack<char>st;
         for(int i=0;i<n;i++){
-
-            if(s[i] =='(' || s[i]=='[' || s[i] == '{'){
+            if(s[i] == '(' ||s[i]=='['||s[i] == '{'){
                 st.push(s[i]);
             }else{
-                if(st.empty()){
-                    return false;
-                }
-                if(st.top() == '(' && s[i] == ')' || 
-                st.top() == '{' && s[i] == '}' ||
-                st.top() == '[' && s[i] == ']' ){
+                if(st.empty()) return false;
+                if((s[i] == ')' && st.top() == '(') || (s[i] == ']' && st.top() == '[') || (s[i] == '}' && st.top() == '{')){
                     st.pop();
                 }else{
                     return false;
                 }
             }
         }
-        if(!st.empty()){
-            return false;
+        if(st.empty()){
+            return true;
         }
-        return true;
+        return false;
     }
 };
