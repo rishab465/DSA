2d5b707daddf9dab488ca4a14bd76ad9cf739ee3
rishab465 <yrishab479@gmail.com>
2026-06-26 20:41:54 +0530

    Time: 35 ms (79.44%) | Memory: 39.5 MB (75.32%) - LeetSync

---
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int count = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            count = 0;
+            for(int j=i;j<n;j++){
+                if(nums[j] == target){
+                    count++;
+                }
+                int len = j-i+1;
+                if(count > len/2){
+                ans++;
+            }
+            }
+            
+        }
+        return ans;
+    }
+};
